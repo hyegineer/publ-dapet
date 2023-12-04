84d2badce4db67e16cc847dd84090b3d786d2cb7
jeonghyejin <hyegineer@gmail.com>
2023-12-05 00:54:04 +0900

    펫시터 의뢰쪽

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -31,6 +31,7 @@
   // 텍스트에어리어: 글자수 체크
   $('.js-txtarea').keyup(function (e) {
     let content = $(this).val();
+    let maxlength = $(this).attr('maxlength');
 
     // 글자수 세기
     if (content.length == 0 || content == '') {
@@ -40,11 +41,11 @@
     }
 
     // 글자수 제한
-    if (content.length > 200) {
+    if (content.length > maxlength) {
       // 200자 부터는 타이핑 되지 않도록
-      $(this).val($(this).val().substring(0, 200));
+      $(this).val($(this).val().substring(0, maxlength));
       // 200자 넘으면 알림창 뜨도록
-      alert('글자수는 200자까지 입력 가능합니다.');
+      alert(`글자수는 ${maxlength}자까지 입력 가능합니다.`);
     };
   });
 
